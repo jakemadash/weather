65a83a3818835b1bf55abb8fe9a84a5f723fc3a4
Jake Ashton <jakemaddenashton@gmail.com>
2022-10-06 10:40:09 -0700

    Write basic function to get weather data from API

---
+++ logic.js
@@ -0,0 +1,9 @@
+async function getWeather(location) {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=854a38f309013f91912348c4a71610bb
+    `,
+    { mode: "cors" }
+  );
+  const weatherData = await response.json();
+  return weatherData;
+}
